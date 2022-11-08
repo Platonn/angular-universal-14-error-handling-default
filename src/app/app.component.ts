a228fc213f985ac257c90063ede270d022fcd0b8
Krzysztof Platis <platonn.git@gmail.com>
2022-11-08 15:44:18 +0100

    add error throwing in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { PlatformLocation } from '@angular/common';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-ng142';
+  queryParams = inject(PlatformLocation).search;
+
+  constructor() {
+    if (this.queryParams.startsWith('?errorIn=AppComponentConstructor')) {
+      setTimeout(() => {
+        throw new Error(this.queryParams);
+      }, 3000);
+    }
+  }
+
+  ngOnInit() {
+    if (this.queryParams.startsWith('?errorIn=AppComponentNgOnInit')) {
+      setTimeout(() => {
+        throw new Error(this.queryParams);
+      }, 3000);
+    }
+  }
 }
