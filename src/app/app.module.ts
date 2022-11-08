362748ef4869598b0b45a51ef1de47a7b3f87b69
Krzysztof Platis <platonn.git@gmail.com>
2022-11-08 15:44:03 +0100

    add error thowing in APP_INITIALIZERs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { PlatformLocation } from '@angular/common';
+import { APP_INITIALIZER, inject, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { delay, map, of } from 'rxjs';
 
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -9,10 +13,43 @@ import { AppComponent } from './app.component';
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     RouterModule.forRoot([{ path: '', component: ChildComponent }]),
   ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' })
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: () => {
+        const queryParams = inject(PlatformLocation).search;
+        return () => {
+          if (queryParams.startsWith('?errorIn=ObservableAppInitializer')) {
+            return of(null).pipe(
+              delay(3000),
+              map(() => {
+                throw new Error(queryParams);
+              })
+            );
+          }
+          return of(null);
+        };
+      },
+    },
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: () => {
+        const queryParams = inject(PlatformLocation).search;
+        return () =>
+          new Promise((resolve, reject) => {
+            if (queryParams.startsWith('?errorIn=PromiseAppInitializer')) {
+              setTimeout(() => {
+                reject(new Error(queryParams));
+              }, 3000);
+            } else {
+              resolve(null);
+            }
+          });
+      },
+    },
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
